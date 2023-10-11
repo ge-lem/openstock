03f5593038088318ac2fcd3b138f9f72991cd62c
Germain Lemasson <germain.lemasson@univ-grenoble-alpes.fr>
2023-10-11 16:36:49 +0200

    fix cas auth optional

--- openstock/settings.py
+++ openstock/settings.py
@@ -74,7 +74,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django_cas_ng.middleware.CASMiddleware',
 ]
 
 
